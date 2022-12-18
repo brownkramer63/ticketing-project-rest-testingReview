d674667ef3299c54a465f5103d94f395f7fa711e
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-12-18 21:45:03 +0100

    part-4-testing - ProjectControllerTest getToken() method and TestResponseDTO added

--- src/test/java/com/cydeo/controller/ProjectControllerTest.java
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.TestResponseDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
 import com.cydeo.enums.Status;
@@ -14,9 +15,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 
@@ -38,7 +42,7 @@ class ProjectControllerTest {
     @BeforeAll
     static void setUp() {
 
-        token = "Bearer " + "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJBUWZjcVVQNGs2UTQ5V3hZa0JTR3p2cUxod2RQVGJrems1UC02N2dxZVNZIn0.eyJleHAiOjE2NzE0MDk2MzIsImlhdCI6MTY3MTM5MTYzMiwianRpIjoiZmFlZjQwOTQtNTE3NS00ODc4LWJlZGYtMTcwZmQ2NDk3NzNjIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2N5ZGVvLWRldiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJkNWE5ZDQxZC0yM2VmLTRmYzAtODVkMC05OTUwNWRlMjc5YWEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ0aWNrZXRpbmctYXBwIiwic2Vzc2lvbl9zdGF0ZSI6IjJiMzNhZTViLTE2NzAtNGI1MS04MjA0LWJmYmNlOTU0NTE0YyIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgxIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiZGVmYXVsdC1yb2xlcy1jeWRlby1kZXYiXX0sInJlc291cmNlX2FjY2VzcyI6eyJ0aWNrZXRpbmctYXBwIjp7InJvbGVzIjpbIk1hbmFnZXIiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiIyYjMzYWU1Yi0xNjcwLTRiNTEtODIwNC1iZmJjZTk1NDUxNGMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoib3p6eSJ9.dIRLo8PIm8xqNCf4QDjMP2pkPv4NM6tmX0dJN8glesacXnbu3e0IYQP-AQvDb_hq6NXuoJTEXOAazJ_3tdvr2OMSZ0AjiirmZYlqZzMyiiKYVd8MoWNRwNOToHw1Em51EWgA4kTSRqpikVYfv6TNO-BM7uooqxh8OKLD7i05enmahWsWmt41DhBt0fxB8-QVzNrJBw8lOdryJKITPIiUQDmZHJyTN6cKnaxEVtpgTLQ8tmE9rO4I4kbBDNAroK_ly6kxj1gmLA-Vr2lW_R7bvyzAs_WfxShzJ6Pq6srxreiZEHYdqaVnyyDjaZIJ6VUrde3dziGrfK9_Dq_YYwfgbw";
+        token = "Bearer " + getToken();
 
         manager = new UserDTO(2L,
                 "",
@@ -128,6 +132,38 @@ class ProjectControllerTest {
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper.writeValueAsString(obj);
+    }
+
+    private static String getToken() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+
+        map.add("grant_type", "password");
+        map.add("client_id", "ticketing-app");
+        map.add("client_secret", "P2tMpbaXrvkdeit4b53umI0lY1p5TtAL");
+        map.add("username", "ozzy");
+        map.add("password", "abc1");
+        map.add("scope", "openid");
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(map, headers);
+
+        ResponseEntity<TestResponseDTO> response =
+                restTemplate.exchange("http://localhost:8080/auth/realms/cydeo-dev/protocol/openid-connect/token",
+                        HttpMethod.POST,
+                        entity,
+                        TestResponseDTO.class);
+
+        if (response.getBody() != null) {
+            return response.getBody().getAccess_token();
+        }
+
+        return "";
+
     }
 
 }
